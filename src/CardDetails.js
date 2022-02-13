153fd7f46e7ee122ee4e3b4bbbf7eae8fa495f3b
Mo Elwekil <Mo@descod.net>
2022-02-13 20:24:19 +0000

    props passed from the index to the card details compnent

--- src/CardDetails.js
+++ src/CardDetails.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const CardDetails = () => {
+const CardDetails = (props) => {
     return (
         <div className="col">
-            <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{ backgroundImage: 'url(https://getbootstrap.com/docs/5.1/examples/features/unsplash-photo-2.jpg)' }}>
+            <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{ backgroundImage: `url(${props.cardImageUrl})` }}>
                 <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                     <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h2>
                     <ul className="d-flex list-unstyled mt-auto">
@@ -12,11 +12,11 @@ const CardDetails = () => {
                         </li>
                         <li className="d-flex align-items-center me-3">
 
-                            <small>Flower</small>
+                            <small>{props.cardType}</small>
                         </li>
                         <li className="d-flex align-items-center">
 
-                            <small>3d</small>
+                            <small>{props.writtenAt}</small>
                         </li>
                     </ul>
                 </div>
--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const App = () => {
                 <h2 className="pb-2 border-bottom">Custom cards</h2>
 
                 <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-                    <CardDetails />
-                    <CardDetails />
-                    <CardDetails />
+                    <CardDetails cardImageUrl="https://getbootstrap.com/docs/5.1/examples/features/unsplash-photo-2.jpg" cardTitle="First Card" writtenAt="3d" cardType="flower" />
+                    <CardDetails cardImageUrl="https://getbootstrap.com/docs/5.1/examples/features/unsplash-photo-2.jpg" cardTitle="First Card" writtenAt="3d" cardType="flower" />
+                    <CardDetails cardImageUrl="https://getbootstrap.com/docs/5.1/examples/features/unsplash-photo-2.jpg" cardTitle="First Card" writtenAt="3d" cardType="flower" />
                 </div>
             </div>
 
